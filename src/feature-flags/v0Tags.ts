7533f4e5a6e7f8b767c2324266f8785ae09b54e6
Ari Palo <ari@aripalo.fi>
2024-11-22 15:43:15 +0200

    feat: show warnings for deprecations

---
+++ src/feature-flags/v0Tags.ts
@@ -0,0 +1,15 @@
+import { Construct } from "constructs";
+
+export const V0_TAGS_CONTEXT_KEY = "@alma-cdk/project:compatibility:v0:tags";
+
+/**
+ * Compatibility flag for v0 tagging behavior.
+ * Due to a bug in v0, the `Contact` and `Organization` tags were NOT applied as they should have.
+ * This flag can be used to enforce behavior that matches v0 implementation:
+ * I.e. `Contact` and `Organization` tags are NOT applied.
+ *
+ * @deprecated This behavior is not encouraged and will be removed in v2.
+ */
+export function useCompatibilityV0Tags(scope: Construct): boolean {
+  return scope.node.tryGetContext(V0_TAGS_CONTEXT_KEY) === true;
+}
