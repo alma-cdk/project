15ef205de02adfdd1a31658a7d69b21bd77be134
Ari Palo <ari@aripalo.fi>
2024-11-22 16:46:26 +0200

    feat: mark the construct as stable

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { awscdk, javascript, TextFile } from "projen";
+import { awscdk, javascript, TextFile, cdk } from "projen";
 import { WorkflowSteps } from "projen/lib/github";
 import { JobPermission } from "projen/lib/github/workflows-model";
 
@@ -19,7 +19,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
   jsiiVersion: "~5.5.0",
   // Metadata
-  stability: "experimental",
+  stability: cdk.Stability.STABLE,
   author: "Alma Media",
   authorOrganization: true,
   authorAddress: "opensource@almamedia.dev",
