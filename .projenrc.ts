9d93f70d68246e2793cde95133d8ee1d65352d69
Ari Palo <679146+aripalo@users.noreply.github.com>
2026-08-10 12:43:56 +0300

    chore: bump major to v3

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,9 @@
 import { AlmaCdkConstructLibrary } from "@alma-cdk/construct-library";
 import { cdk } from "projen";
 
+const MAJOR_VERSION = 3;
+const NEXT_MAJOR_VERSION = MAJOR_VERSION + 1;
+
 const project = new AlmaCdkConstructLibrary({
   name: "@alma-cdk/project",
   author: "Alma Media",
@@ -8,10 +11,16 @@ const project = new AlmaCdkConstructLibrary({
   description: "Opinionated CDK Project “Framework”",
   repositoryUrl: "https://github.com/alma-cdk/project.git",
   stability: cdk.Stability.STABLE,
-  majorVersion: 3,
+  majorVersion: MAJOR_VERSION,
   devDeps: ["@types/nunjucks"],
   bundledDeps: ["change-case", "nunjucks"],
   releaseEnvironment: "production",
+  releaseBranches: {
+    [`${NEXT_MAJOR_VERSION}.x`]: {
+      majorVersion: NEXT_MAJOR_VERSION,
+      prerelease: "beta",
+    },
+  },
   sonarProjectPropertiesExtraLines: [
     "sonar.issue.ignore.multicriteria=e1,e2",
     "sonar.issue.ignore.multicriteria.e1.ruleKey=typescript:S1874",
